39db75232fe6cfe5ebfd4943a61f16f272e8b17d
sabharw8 <kokisabharwal@hotmail.com>
2014-07-22 17:14:49 -0400

    Update header.php

--- under/header.php
+++ under/header.php
@@ -11,9 +11,14 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title>
+<?php 
+
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url'); ?>">
+
+
 
 <?php wp_head(); ?>
 </head>
@@ -24,8 +29,10 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+		<div class="title-box">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			</div>
 		</div>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
